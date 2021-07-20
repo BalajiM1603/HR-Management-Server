d889d20009c4aad3015a573fece701440d1eabcc
Balaji M <51756211+BalajiM1603@users.noreply.github.com>
2021-07-20 11:08:03 +0530

    Update database.config.js

--- config/database.config.js
+++ config/database.config.js
@@ -1,7 +1,4 @@
 module.exports = {
-    // url: 'mongodb+srv://venkat:demo134@cluster0.6rmk0.mongodb.net/test'
-    //url:'mongodb+srv://venkat:demo1234@cluster0.6rmk0.mongodb.net/test?retryWrites=true&w=majority'
-    //url: 'mongodb://localhost:27017'
-    url: 'mongodb+srv://Balaji:Bm1998@1603@cluster0.uk4rv.mongodb.net/test'
+    url: 'mongodb://localhost:27017'
 }
 
